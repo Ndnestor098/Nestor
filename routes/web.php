2718beb06f582f73a05ca45d38a6114e19546532
CatMaster <trabajo.nestor.098@gmail.com>
2024-11-28 17:29:06 +0100

    Eliminar rutas no usadas

--- routes/web.php
+++ routes/web.php
@@ -30,10 +30,4 @@ Route::post('/certificate', [CertificateController::class, "create"])->name("cer
 
 Route::delete('/certificate/{id}', [CertificateController::class, "delete"])->name("certificate.delete");
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
 require __DIR__.'/auth.php';
